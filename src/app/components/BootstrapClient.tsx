ebd2f0e1f3c3bef9a8f8ae90daaad6b37bb342ac
Aliza-Gopang <149888499+Aliza-Gopang@users.noreply.github.com>
2025-06-30 07:22:58 -0800

    Fix bootstrap import for Vercel

--- src/app/components/BootstrapClient.tsx
+++ src/app/components/BootstrapClient.tsx
@@ -1,11 +1,12 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export default function BootstrapClient() {
   useEffect(() => {
-    // @ts-ignore: No type definitions for bootstrap.bundle.min.js
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    import('bootstrap').then((bootstrap) => {
+      (window as any).bootstrap = bootstrap;
+    });
   }, []);
 
   return null;
